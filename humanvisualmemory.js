87e0284572d60aae87ef409f3a0af2b2f6d30c47
Xines <denflottejonas@hotmail.com>
2023-06-22 01:08:54 +0200

    Update humanvisualmemory.js
    Increase the accuracy.

--- humanvisualmemory.js
+++ humanvisualmemory.js
@@ -4,7 +4,6 @@ var evt = document.createEvent('MouseEvents');
 evt.initMouseEvent('mousedown', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
 
 var StartBtn = document.querySelector("button:nth-child(1)");
-var allWindows = document.getElementsByClassName("css-lxtdud eut2yre1");
 var windowsActiveMemory = [];
 var curActiveMemory;
 
@@ -15,16 +14,16 @@ function run() {
   setInterval(() => {
     checkAndClick();
     updateNewMemory();
-    console.log('RUN ACTIVE');
-  }, 600);
+  }, 50);
 }
 
 function checkAndClick() {
-  if (windowsActiveMemory.length > 0) {
-    for (var i = 0; i < windowsActiveMemory.length; i++) {
-      windowsActiveMemory[i].dispatchEvent(evt);
-    }
-  }
+	if (windowsActiveMemory.length > 0) {
+		for (var i = 0; i < windowsActiveMemory.length; i++) {
+			windowsActiveMemory[i].dispatchEvent(evt);
+		}
+		windowsActiveMemory = [];
+	}
 }
 
 function updateNewMemory() {
